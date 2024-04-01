4ff7d4e7bc0f3ea0e0ac2d3aa398c6c5062646b9
seheichenko-anna <seheichenko.anna@gmail.com>
2024-04-01 19:33:13 +0300

    Updates

--- assets/Catalog-BV3vpzPC.js
+++
@@ -1,1 +0,0 @@
-import{u as t,r as s,f as r,j as o}from"./index-DcX8Z4dj.js";import{C as e}from"./CarsCatalog-DcOQENNa.js";import"./Button-C4CmLN6a.js";const f=()=>{const a=t();return s.useEffect(()=>{a(r({page:1,make:null}))},[a]),o.jsx(e,{type:"catalog"})};export{f as default};
