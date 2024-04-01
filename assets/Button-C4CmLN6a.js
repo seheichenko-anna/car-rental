4988c917b0a3505993bf5061c2e653b2ecce7a29
seheichenko-anna <seheichenko.anna@gmail.com>
2024-04-01 19:03:13 +0300

    Updates

---
+++ assets/Button-C4CmLN6a.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DcX8Z4dj.js";const r="_btn_1j65z_1",b={btn:r},c=({children:t,onClick:n,style:s})=>o.jsx("button",{className:b.btn,onClick:n,style:s,children:t});export{c as B};
