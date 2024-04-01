db0701207bccbb4f5285280c75fd0201a95ca8b5
seheichenko-anna <seheichenko.anna@gmail.com>
2024-04-01 20:14:21 +0300

    Updates

---
+++ assets/Catalog-mTLmRa3q.js
@@ -0,0 +1,1 @@
+import{u as t,r as s,f as r,j as o}from"./index-BXrFTJcF.js";import{C as e}from"./CarsCatalog-CPTMr3Ku.js";import"./Button-CEBcsJFw.js";const f=()=>{const a=t();return s.useEffect(()=>{a(r({page:1,make:null}))},[a]),o.jsx(e,{type:"catalog"})};export{f as default};
